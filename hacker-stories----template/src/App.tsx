3799af5bfc94ab3e5c36c294a73f128441a39b64
amanueldemirew <amanuel.demirew@yahoo.com>
2024-09-08 16:53:13 +0300

    props handeling: destructure, nested, spreade and rest

--- hacker-stories----template/src/App.tsx
+++ hacker-stories----template/src/App.tsx
@@ -29,59 +29,69 @@ const App = () => {
     },
   ];
 
+  const [searchTerm, setSearchTerm] = React.useState("React");
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search />
+      <Search search={searchTerm} onSearch={handleSearch} />
 
       <hr />
 
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 };
 
-const Search = () => {
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // synthetic event
-    console.log(event);
-    // value of target (here: input HTML element)
-    console.log(event.target.value);
-  };
-
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-    </div>
-  );
+type SearchProps = {
+  search: string;
+  onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
+
+const Search = ({ search, onSearch }: SearchProps) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" value={search} onChange={onSearch} />
+  </div>
+);
 
 type ListProps = {
   list: Story[];
 };
 
-const List = (props: ListProps) => (
+const List = ({ list }: ListProps) => (
   <ul>
-    {props.list.map((item) => (
-      <Item key={item.objectID} item={item} />
+    {list.map(({objectID, ...item}) => (
+      <Item key={objectID} {...item} />
     ))}
   </ul>
 );
 
 type ItemProps = {
-  item: Story;
+  title: string;
+  url: string;
+  author: string;
+  num_comments: number;
+  points: number;
 };
 
-const Item = (props: ItemProps) => (
+const Item = ({ title, url, author, num_comments, points }: ItemProps) => (
   <li>
     <span>
-      <a href={props.item.url}>{props.item.title}</a>
+      <a href={url}>{title}</a>
     </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
   </li>
 );
 
