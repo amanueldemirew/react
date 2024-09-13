74ef50e09d3c2071046b10abf1ef92c6a21623aa
amanueldemirew <amanuel.demirew@yahoo.com>
2024-09-13 08:10:12 +0300

    learninh curve

---
+++ react-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const App = () => {
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  useState(
+    {
+      
+    }
+  )
+  const [isLoading, setLoading] = useState(false);
+  return <div></div>;
+};
+
+export default App;
